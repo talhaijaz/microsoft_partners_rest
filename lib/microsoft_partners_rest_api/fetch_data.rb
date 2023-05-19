22aad2e8b4de4066ff680e295b000ab39e4d96f8
talhaijaz <talhaijaz123451@gmail.com>
2023-05-19 18:39:17 +0500

    feat: Added Subscription Analytics Api

--- lib/microsoft_partners_rest_api/fetch_data.rb
+++ lib/microsoft_partners_rest_api/fetch_data.rb
@@ -5,7 +5,7 @@ module MicrosoftPartnersRestApi
 
     def initialize(body, client = MicrosoftPartnersRestApi.client)
       @config = MicrosoftPartnersRestApi.config
-      @api_url = 'https://api.partnercenter.microsoft.com/v1'
+      @api_url = 'https://api.partnercenter.microsoft.com'
       @body = body.symbolize_keys
       @access_token = fetch_access_token
       @client = client
@@ -61,16 +61,18 @@ module MicrosoftPartnersRestApi
         fetch_customer_products(body[:customer_id], body[:target_view])
       when 'ProductSku'
         fetch_product_sku(body[:product_id], body[:sku_id], body[:country_code])
+      when 'SubscriptionAnalytics'
+        fetch_subscription_analytics(body[:filter])
       end
     end
 
     def fetch_customers
-      url = api_url + "/customers"
+      url = api_url + "/v1/customers"
       api_call(url)
     end
 
     def fetch_invoices
-      url = api_url + "/invoices"
+      url = api_url + "/v1/invoices"
       api_call(url)
     end
 
@@ -92,7 +94,7 @@ module MicrosoftPartnersRestApi
       return error_response('InvoiceId, Provider, and InvoiceLIneItemType should be present') unless invoice_id.present? &&
         body[:provider].present? && body[:invoicelineitemtype].present?
          
-      url = api_url + "/invoices/#{invoice_id}/lineitems?provider=#{body[:provider]}&invoicelineitemtype=#{body[:invoicelineitemtype]}"
+      url = api_url + "/v1/invoices/#{invoice_id}/lineitems?provider=#{body[:provider]}&invoicelineitemtype=#{body[:invoicelineitemtype]}"
       api_call(url)
     end
 
@@ -143,7 +145,13 @@ module MicrosoftPartnersRestApi
       return error_response('ProductId, SkuId or CountryCode is missing') unless product_id.present? ||
         country_code.present? || sku_id.present?
 
-      url = api_url + "/products/#{product_id}/skus/#{sku_id}?country=#{country_code}"
+      url = api_url + "/v1/products/#{product_id}/skus/#{sku_id}?country=#{country_code}"
+      api_call(url)
+    end
+    
+    def fetch_subscription_analytics(filter)
+      url = api_url + '/partner/v1/analytics/subscriptions'
+      url = url + "?filter=#{filter}" if filter.present?
       api_call(url)
     end
 
@@ -160,7 +168,7 @@ module MicrosoftPartnersRestApi
     end
 
     def customer_specific_api_url(customer_id, entity_path)
-      api_url + "/customers/#{customer_id}/#{entity_path}"
+      api_url + "/v1/customers/#{customer_id}/#{entity_path}"
     end
 
     def valid_target_view_options
